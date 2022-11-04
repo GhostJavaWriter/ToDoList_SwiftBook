da3274f3cbfcca84a47dcc1f3849ba525ab528ef
Bair <40758299+GhostJavaWriter@users.noreply.github.com>
2022-11-04 21:42:58 +0800

    add: properties and configure titleLabel

--- ToDoList_1/ViewController.swift
+++ ToDoList_1/ViewController.swift
@@ -8,13 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var titleLabel = UILabel()
+    private var warningLabel = UILabel()
+    private var emailTextField = UITextField()
+    private var passwordTextField = UITextField()
+    private var loginButton = UIButton()
+    private var registerButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        view.backgroundColor = .systemBlue
+        configureSubviews()
     }
 
-
+    private func configureSubviews() {
+        
+        let fontName = "Tamil Sangam MN"
+        let titleFontSize: CGFloat = 40
+        let labelsTextColor = UIColor.white
+        
+        view.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.textColor = labelsTextColor
+        titleLabel.font = UIFont(name: fontName, size: titleFontSize)
+        titleLabel.text = "To do list"
+        
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
+    }
 }
 
