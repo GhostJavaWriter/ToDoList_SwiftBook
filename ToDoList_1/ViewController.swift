5c6244c7f0f27b4f04fc68ab2e8d58a10fc05000
Bair <40758299+GhostJavaWriter@users.noreply.github.com>
2022-11-04 20:59:27 +0800

    initial commit

---
+++ ToDoList_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList_1
+//
+//  Created by Bair Nadtsalov on 4.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
